8997f09a8816190cc373aed8136daee11469476d
Francisco Reyes <juan271292@hotmail.com>
2018-04-22 16:11:36 -0500

    Se agrega POGO codigo postal

---
+++ src/main/groovy/com/fco271292/domain/CodigoPostal.groovy
@@ -0,0 +1,12 @@
+package com.fco271292.domain
+
+class CodigoPostal {
+	
+	String codigoPostal
+	String descripcionAsentamiento
+	String tipoAsentamiento
+	String nombreMunicipio
+	String nombreEstado
+	String nombreCiudad
+	
+}
