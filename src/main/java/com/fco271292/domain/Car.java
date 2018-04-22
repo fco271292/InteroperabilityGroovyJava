234b26cf9ab62ec97652fb56ef736e82d4aa0264
Francisco Reyes <juan271292@hotmail.com>
2018-04-22 10:17:09 -0500

    Se quita anotacion de lombock

--- src/main/java/com/fco271292/domain/Car.java
+++ src/main/java/com/fco271292/domain/Car.java
@@ -3,13 +3,10 @@ package com.fco271292.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-//@RequiredArgsConstructor
 public class Car {
 	
 	Integer id;
